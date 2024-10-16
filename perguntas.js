67740c18ff5390fe6270b0d49c87e89f7bb15a9f
milenabarboza <160685977+milenabarboza@users.noreply.github.com>
2024-10-15 21:56:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,16 @@
+criaCartao(
+   'programação',
+   'O que é JavaScript?',
+   'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia'
+    'Qual a capital da frança?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação'
+    'O que é uma função?'
+    'Uma função é um bloco de código que executa alguma tarefa'
